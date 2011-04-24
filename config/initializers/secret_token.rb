9251dc92fb9920c442e6233645deae0ad3ac9401
Mike Shea <mike@mikeshea.net>
2011-04-23 21:57:49 -0400

    First Working Build with Authentication

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CheckIn::Application.config.secret_token = '07418a95b9405892052703bab6e49bc5197b389ee3d31bfbb6abe845f7e08cc671b666b3a1a2c9f9b124e7bd53d7c9020052760521220be89d0ee058a3d35e9b'
